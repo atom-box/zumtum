dedab28ca9315c07dbd7599dc8306d1cdbe89458
Evan Genest <genest.1@osu.edu>
2019-05-02 07:32:35 -0500

    Move headers up to the top and fix the top formatting.

--- reqSpotlightServer.js
+++ reqSpotlightServer.js
@@ -1,17 +1,17 @@
 const http = require('http');
 const digitalOcean =(req, res)=>{
 	let enlargedURL = '<div style="border: 5px solid #999;"><h1 style="font-family: arial; color: green; font-size: 7rem ">' +req.url+  '</h1></div>';
-	let message = '<h1>In pure Node.js, the request object only has two properties!</h1><h3 style="font-family: monospace"> ';
-	message += 'Headers has this many things in its object: ' + req.headers + '<br>' ;
-	message += 'URL is: ' + req.url + '<br>';
+	var headerThings = '';
+	for(var name in req.headers) headerThings += name + ': ' + req.headers[name] + '       ';
+	let styling = '<style type="text/css">:root{--lightGray:#efeff4;--darkGray:#4c4d4b;--skyColor:#7cacff;--palerSky:#a5dcff;--infieldGreen:#34931c;--tigersNavy:#071735}.title__hoegarden{text-shadow:1px 1px 2px black,0 0 25px gray,0 0 5px black;font-family:monospace;color:white;font-weight:500}.title__skinny{font-weight:100;font-size:.8rem;text-indent:2.5rem;line-height:2.6rem}.elShadow{box-shadow:0 7px 7px 0 rgba(0,0,0,0.2),0 6px 20px 0 rgba(0,0,0,0.19);border:.1rem hidden;border-radius:.3rem}.greentext{color:var(--infieldGreen)}h1, h2, h3, h4, h5{ font-family: verdana; }p{font-family: monospace}</style>';
+	let message = styling + '<h1>In pure Node.js, the request object only has two properties!</h1><h2> ';
+	message += '1) Headers:</h2><p>'+ headerThings + '</p>' ;
+	message += '<h2>2) The URL is:</h2><p> ' + req.url + '<br></p><h2>';
 	message += '...thats it.';
-	message += '</h3>';
+	message += '</h2>';
 	message += enlargedURL;
 
-	// try printing the request headers req.headers
-	var s = '';
-	for(var name in req.headers) s += name + ': ' + req.headers[name] + '/n';
-	message += 'New Part: ' + s;
+	
 
 	switch(req.url.length){
 		case 0:
