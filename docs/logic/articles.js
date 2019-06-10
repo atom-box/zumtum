73e6bc275c06b649ba1e41c85bb28b682de1dfd4
Evan Genest <genest.1@osu.edu>
2019-06-10 18:28:44 -0500

    Set up two scripts to feed in a list of three articles from a JSON object.

---
+++ docs/logic/articles.js
@@ -0,0 +1,32 @@
+export {z, y, x};
+var x = 5.4321;
+var y =  [ 'border', 'dodgerBlue',
+'subject',   'evolution',
+'title', 'Icefish Study Adds Another Color to the Story of Blood',
+'author','John Rennie',
+'content', 'The rainbow of pigments that animals use for blood illustrates a central truth of evolution.'];
+var z = JSON.stringify(
+
+[{'border': 'dodgerBlue',
+'subject':   'evolution',
+'title': 'Icefish Study Adds Another Color to the Story of Blood',
+'author': 'John Rennie',
+'content': 'The rainbow of pigments that animals use for blood illustrates a central truth of evolution.'},     
+
+{ 
+'border': 'gray',
+'subject': 'evolution',
+'title': 'Researchers Rethink the Ancestry of Complex Cells',
+'author': 'Christie Wilcox',
+'content':  'New studies revise ideas about the symbiosis that gave mitochondria to cells and about whether the last common ancestor of all eukaryotes was one cell or many.'
+}, 
+{ 
+'border': 'gray',
+'subject': 'astro-geology',
+'title': 'The Scientist Who Cooks Up the Skies of Faraway Worlds',
+'author': 'Shannon Hall',
+'content': 'Astronomers will soon take their first glance at the atmosphere of a distant exoplanet. Sarah Hörst is writing the guidebook for these exoplanetary explorers, one that will reveal what a distinctive atmosphere says about the world underneath.'
+}
+,{}],);
+
+// };
---
+++ docs/logic/top.js
@@ -0,0 +1,34 @@
+        import {z} from './articles.js';
+
+        console.log(z[7]);
+
+        function clipArticle(){
+            alert("TODO: 1) once articles are served from the static pseudo db 2) make a function to send to the system's clipboard");
+        }
+
+        // 001 CHANGER
+        let elInfo001 = document.getElementById("footer__logo");
+        elInfo001.innerHTML = "starch";
+        elInfo001.style.color = "yellow";
+        console.log('Last line.');
+
+        //002 ONCLICK ONMOUSEOVER in the HTML itself
+
+        //003 APPEND
+        /*
+        make a mnemonic: five steps: create el, create child, append child , into a var get element, append child again
+        */
+        function asidifier(){
+            let wikDiv = document.createElement("aside");
+            let wikTxt = document.createTextNode("Evolution is change in the heritable characteristics of biological populations over successive generations.");
+            wikDiv.appendChild(wikTxt);
+            let roofEl = document.getElementById("article001");
+            roofEl.insertAdjacentElement("afterend", wikDiv);
+        }
+
+        //004 Read in your data.  
+
+
+
+
+// asidifier();
