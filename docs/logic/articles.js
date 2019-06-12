b7c7e10dcd812c3cc93f2b4565b3898694fe8f73
Evan Genest <genest.1@osu.edu>
2019-06-12 09:55:30 -0500

    Rename SUBJECT to TOPIC.

--- docs/logic/articles.js
+++ docs/logic/articles.js
@@ -2,22 +2,23 @@ export {z};
 
 var z = JSON.stringify(
 
-[{'border': 'dodgerBlue',
-'subject':   'evolution',
+[{'id': '4338',
+	'border': 'dodgerBlue',
+'topic':   'evolution',
 'title': 'Icefish Study Adds Another Color to the Story of Blood',
 'author': 'John Rennie',
 'content': 'The rainbow of pigments that animals use for blood illustrates a central truth of evolution.'},     
 
-{ 
+{'id': '4651', 
 'border': 'gray',
-'subject': 'evolution',
+'topic': 'evolution',
 'title': 'Researchers Rethink the Ancestry of Complex Cells',
 'author': 'Christie Wilcox',
 'content':  'New studies revise ideas about the symbiosis that gave mitochondria to cells and about whether the last common ancestor of all eukaryotes was one cell or many.'
 }, 
-{ 
+{'id': '8001', 
 'border': 'gray',
-'subject': 'astro-geology',
+'topic': 'astro-geology',
 'title': 'The Scientist Who Cooks Up the Skies of Faraway Worlds',
 'author': 'Shannon Hall',
 'content': 'Astronomers will soon take their first glance at the atmosphere of a distant exoplanet. Sarah Hörst is writing the guidebook for these exoplanetary explorers, one that will reveal what a distinctive atmosphere says about the world underneath.'
