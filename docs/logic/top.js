b5cfd76a5058ae9f359f6c62c9874079f21dde9f
Evan Genest <genest.1@osu.edu>
2019-06-13 16:25:23 -0500

    Put the article getting into a GETARTICLES function.

--- docs/logic/top.js
+++ docs/logic/top.js
@@ -1,34 +1,40 @@
-        import {z} from './articles.js';
-        console.log(z[7]);
-        let articles = JSON.parse(z);
-        console.log(articles[1].author); 
+import {z} from './articles.js';
 
-        function clipArticle(){
-            alert("TODO: 1) once articles are served from the static pseudo db 2) make a function to send to the system's clipboard");
-        }
-
+function getArticles(){
+    let articles = JSON.parse(z);
+    console.log(articles[1].author); 
+    return articles;
+}
 
 
 
-        // 001 CHANGER
-        let elInfo001 = document.getElementById("footer__logo");
-        elInfo001.innerHTML = "starch";
-        elInfo001.style.color = "yellow";
-        console.log('Last line.');
 
-        //002 ONCLICK ONMOUSEOVER in the HTML itself
 
-        //003 APPEND
-        /*
-        make a mnemonic: five steps: create el, create child, append child , into a var get element, append child again
-        */
-        function asidifier(){
-            let wikDiv = document.createElement("aside");
-            let wikTxt = document.createTextNode("Evolution is change in the heritable characteristics of biological populations over successive generations.");
-            wikDiv.appendChild(wikTxt);
-            let roofEl = document.getElementById("article001");
-            roofEl.insertAdjacentElement("afterend", wikDiv);
-        }
+function clipArticle(){  
+    // RIGHT THUMB in the BUTTONS
+    alert("TODO: 1) once articles are served from the static pseudo db 2) make a function to send to the system's clipboard");
+}
+
+
+// 001 CHANGER
+let elInfo001 = document.getElementById("footer__logo");
+elInfo001.innerHTML = "starch";
+elInfo001.style.color = "yellow";
+console.log('Last line.');
+
+
+
+//003 APPEND
+/*
+make a mnemonic: five steps: create el, create child, append child , into a var get element, append child again
+*/
+function asidifier(){
+    let wikDiv = document.createElement("aside");
+    let wikTxt = document.createTextNode("Evolution is change in the heritable characteristics of biological populations over successive generations.");
+    wikDiv.appendChild(wikTxt);
+    let roofEl = document.getElementById("article001");
+    roofEl.insertAdjacentElement("afterend", wikDiv);
+}
 
         //004 Display articles from the JSON
         
@@ -69,14 +75,20 @@ function showArticles(i, a){
     document.getElementById(i).innerHTML = html;
 }
 
-function addArticle(){
+function addArticle(){  /*************************************************/
     let submission = document.getElementById('newArticle').value;
     let now = new Date();
     console.log( now.getSeconds() +  '  ' + submission );
+
 }
 
 
- 
+// LISTEN TO THE SUBMIT ARTICLE BUTTON
 document.getElementById('submit').addEventListener('click', addArticle);
+
+// APPEND ALL THE ACTUAL DIVS 
 showArticles('insert001', articles);
+
+
+
 // asidifier();
