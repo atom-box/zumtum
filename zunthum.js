63e620585b1bfffe5906cd45904485f69d15f456
Evan Genest <genest.1@osu.edu>
2019-06-18 10:50:29 -0500

    Tiny server. Serves: 404, 500, two routes.

--- zunthum.js
+++ zunthum.js
@@ -5,17 +5,27 @@ let app = express();
 
 const PORT = 2001;
 
+app.get('/', function(req, res){
+	res.type('text/plain');
+	res.send('Open for business.')
+});
+
+app.get('/frogs', function(req, res){
+	res.type('text/plain');
+	res.send('This is a special page, called FRONGS');
+});
+
 app.use(function(req, res){
 	res.type('text/plain');
-	res.status(500)
-	res.send('Server says, "My bad."')
+	res.status(404);
+	let now = new Date();
+	res.send(`Your bad. At ${now}.` );
 });
 
 app.use((err, req, res, next)=>{
 	res.type('text/plain')
-	res.status(400);
-	let now = new Date();
-	res.send(`Your bad. At ${now}.` );
+	res.status(500);
+	res.send('Server says, "My bad."')
 })
 
 
