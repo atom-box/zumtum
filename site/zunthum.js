788f8a3d49db874c309611d1f3dd844915f3f7e9
Evan Genest <genest.1@osu.edu>
2019-06-18 10:04:08 -0500

    Wrote from memory mostly the tss and SUUL. Just serves 500 and 400 errors.

--- site/zunthum.js
+++ site/zunthum.js
@@ -4,9 +4,21 @@ let express = require('express');
 let app = express();
 
 const PORT = 2001;
-// app.set
-// app.use
-// app.use
+
+app.use(function(req, res){
+	res.type('text/plain');
+	res.status(500)
+	res.send('Server says, "My bad."')
+});
+
+app.use((err, req, res, next)=>{
+	res.type('text/plain')
+	res.status(400);
+	let now = new Date();
+	res.send(`Your bad. At ${now}.` );
+})
+
+
 app.listen(PORT, function(){
 	console.log(`Listening on Port ${PORT}. Press Ctl-C to quit.`);
 });
