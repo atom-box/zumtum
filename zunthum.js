8d553fb32fa82c1518c295c8f0a93add3d642b4f
Evan Genest <genest.1@osu.edu>
2019-06-28 20:29:42 -0500

    Write the server to dish up five handlebar views:  APP.RENDER.:

--- zunthum.js
+++ zunthum.js
@@ -6,21 +6,33 @@ let app = express();
 const PORT = 2001;
 
 app.get('/', function(req, res){
-	res.type('text/plain');
-	res.send('Open for business.')
+	res.render('home');
 });
 
-app.get('/frogs', function(req, res){
-	res.type('text/plain');
-	res.send('This is a special page, called FRONGS');
+app.get('/signin', function(req, res){
+	res.render('signin')
 });
 
-app.use(function(req, res){
+app.get('/stepA', function(req, res){
+	res.render('stepA');
+});
+
+app.get('/stepB', function(req, res){
+	res.render('stepB');
+});
+
+app.get('/stepC', function(req, res){
+	res.render('stepC');
+});
+
+app.use(function(req, res, next){
 	res.type('text/plain');
 	res.status(404);
 	let now = new Date();
 	res.send(`Your bad. At ${now}.` );
 });
+
+// DOUBLECHECK WHICH HAS NEXT
 
 app.use((err, req, res, next)=>{
 	res.type('text/plain')
