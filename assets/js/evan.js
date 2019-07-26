627b653914609a0d72a41241d3dfac9dd0ed34e3
Evan Genest <genest.1@osu.edu>
2019-07-26 06:56:26 -0500

    Move the js out to its own file.

---
+++ assets/js/evan.js
@@ -0,0 +1,26 @@
+/**         INSTRUCTIONS TOGGLE     **/
+        var el1 = document.getElementById('instructions-place');
+    
+        function press(){
+            el1.innerHTML = "Lather, Rinse, Repeat";
+        }
+
+/**      D A T A B A S E todo! go to SQL   **/
+
+// Array of 
+// 0  Instructions
+// 1, 2, ...   
+var messages = []; 
+
+// String of
+// inputted text (giant, from the user or the options menu)
+
+// Array of 
+// 0, 1, 2, ...  Parsed out of user input string
+var passages = []; 
+
+// Array of 
+// 0, 1, 2, ...  Written by the user. 
+// Named in tribute to Mike Hernandez
+var questionings = []; 
+
